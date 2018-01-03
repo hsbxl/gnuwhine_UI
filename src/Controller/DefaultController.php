53207ef43717935768123e33db01b78d6962b624
betz <tom@behets.me>
2018-01-03 17:26:42 +0100

    filter double recipes

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,7 +3,8 @@
 namespace Drupal\gnuwhine_ui\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use GitWrapper\GitWrapper;
+use Gitonomy\Git\Repository;
+use Symfony\Component\Yaml\Yaml;
 
 /**
  * Class DefaultController.
@@ -18,15 +19,15 @@ class DefaultController extends ControllerBase {
    */
   public function fork() {
     $client = new \Github\Client();
-    $client->authenticate('fd63540c7408f6be9a9533beee9f808586ae1202', NULL, 'http_token');
-    $recipes = [];
+    $client->authenticate('6db80d761d77ed184fa1451f8018566a5ed84515', NULL, 'http_token');
+    $recipe_branches = [];
 
-    $username = '0x20';
-    $repo = 'HTH';
+    $username = 'hsbxl';
+    $repo = 'gnuwhine';
 
     $branches = $client->api('repo')->branches($username, $repo);
     foreach ($branches as $branch) {
-      $recipes['0x20/HTH']['branches'][] = $branch['name'];
+      $recipe_branches['hsbxl/gnuwhine']['branches'][] = $branch['name'];
     }
 
     $forks = $client->api('repo')->forks()->all($username, $repo);
@@ -36,13 +37,39 @@ class DefaultController extends ControllerBase {
 
       $branches = $client->api('repo')->branches($username, $repo);
       foreach ($branches as $branch) {
-        $recipes[$fork['full_name']]['branches'][] = $branch['name'];
+        $recipe_branches[$fork['full_name']]['branches'][] = $branch['name'];
       }
     }
 
-    $gitWrapper = new GitWrapper();
+    foreach ($recipe_branches as $key => $recipe) {
+      if(!file_exists('/tmp/' . $key)) {
+        $url = 'https://github.com/' . $key . '.git';
+        \Gitonomy\Git\Admin::cloneTo('/tmp/' . $key, $url, FALSE);
+      }
 
-    ksm($recipes);
+      foreach ($recipe['branches'] as $branch) {
+        $repository = new \Gitonomy\Git\Repository('/tmp/' . $key);
+        $repository->run('fetch');
+        $repository->run('checkout', [$branch]);
+
+        $recipe = Yaml::parse(file_get_contents('/tmp/' . $key . '/recipe.yaml'));
+
+        if(!empty($filtered_recipes)) {
+          $double = FALSE;
+          foreach ($filtered_recipes as $filtered_recipe) {
+            if($recipe === $filtered_recipe) {
+              $double = TRUE;
+            }
+          }
+          if(!$double) {$filtered_recipes[$key . '::' . $branch] = $recipe;}
+        }
+        else {
+          $filtered_recipes[$key . '::' . $branch] = $recipe;
+        }
+      }
+    }
+
+    ksm($filtered_recipes);
 
     return [
       '#type' => 'markup',
